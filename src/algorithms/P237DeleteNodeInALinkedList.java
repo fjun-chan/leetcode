62580923f8e6c89c326f25b6b652c128aafdf515
fjun <fjun2008@gmail.com>
2016-04-04 20:33:15 +0800

    Delete Node in a Linked List
    Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.

---
+++ src/algorithms/P237DeleteNodeInALinkedList.java
@@ -0,0 +1,31 @@
+package algorithms;
+
+/**
+ * 237. Delete Node in a Linked List
+ *
+ * Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+ *
+ *  Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3,
+ *  the linked list should become 1 -> 2 -> 4 after calling your function.
+ *
+ * Created by fjun on 4/4/16.
+ */
+public class P237DeleteNodeInALinkedList {
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+    public void deleteNode(ListNode node) {
+        ListNode next = node.next;
+        if (next == null) {
+            return;
+        }
+        node.val = next.val;
+        node.next = next.next;
+    }
+}
