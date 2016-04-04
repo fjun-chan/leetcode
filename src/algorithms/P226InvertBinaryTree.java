058ea0b0a94e383a9dead2d1b0019cf384d39165
fjun <fjun2008@gmail.com>
2016-04-04 18:13:47 +0800

    Invert a binary tree.

---
+++ src/algorithms/P226InvertBinaryTree.java
@@ -0,0 +1,51 @@
+package algorithms;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ *  Invert a binary tree.
+
+ 4
+ /   \
+ 2     7
+ / \   / \
+ 1   3 6   9
+
+ to
+
+ 4
+ /   \
+ 7     2
+ / \   / \
+ 9   6 3   1
+
+ *
+ * Created by fjun on 4/4/16.
+ */
+public class P226InvertBinaryTree {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode(int x) { val = x; }
+     * }
+     */
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        TreeNode right = null;
+        if (root.right != null) {
+            right = invertTree(root.right);
+        }
+        TreeNode left = null;
+        if (root.left != null) {
+            left = invertTree(root.left);
+        }
+        root.right = left;
+        root.left = right;
+        return root;
+    }
+}
