326cbe9fb20c6584f2fbaa58d0fe854ceb471c22
fjun <fjun2008@gmail.com>
2016-04-05 13:14:13 +0800

    144 Binary Tree Preorder Traversal.

---
+++ src/algorithms/P144BinaryTreePreorderTraversal.java
@@ -0,0 +1,46 @@
+package algorithms;
+
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given a binary tree, return the preorder traversal of its nodes' values.
+ * <p>
+ * For example:
+ * Given binary tree {1,#,2,3},
+ * 1
+ * \
+ * 2
+ * /
+ * 3
+ * return [1,2,3].
+ * <p>
+ * Note: Recursive solution is trivial, could you do it iteratively?
+ * <p>
+ * Created by fjun on 4/5/16.
+ */
+
+public class P144BinaryTreePreorderTraversal {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<Integer>();
+        }
+
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(root.val);
+        list.addAll(preorderTraversal(root.left));
+        list.addAll(preorderTraversal(root.right));
+        return list;
+
+    }
+}
