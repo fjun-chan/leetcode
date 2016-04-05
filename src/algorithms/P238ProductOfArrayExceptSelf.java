5c079476f6ed650d206398fbc7e3aee8c978fb62
fjun <fjun2008@gmail.com>
2016-04-05 11:30:48 +0800

    238 Product of Array Except Self.
    计算数组中，除自身外，其它数据的积。

---
+++ src/algorithms/P238ProductOfArrayExceptSelf.java
@@ -0,0 +1,33 @@
+package algorithms;
+
+import java.util.Arrays;
+
+/**
+ * Given an array of n integers where n > 1, nums, return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+ * Solve it without division and in O(n).
+ *
+ * For example, given [1,2,3,4], return [24,12,8,6].
+ *
+ * Created by fjun on 4/5/16.
+ */
+public class P238ProductOfArrayExceptSelf {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(productExceptSelf(new int[]{1, 2, 3, 4})));
+    }
+
+    public static int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        int right = 1;
+        for (int i = nums.length - 1; i >= 0; i --) {
+            res[i] = right;
+            right *= nums[i];
+        }
+
+        int left = 1;
+        for (int i = 0; i < nums.length; i ++) {
+            res[i] *= left;
+            left *= nums[i];
+        }
+        return res;
+    }
+}
