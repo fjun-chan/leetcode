a32f98d61237d1cda0bea38953a9bcee61f12399
fjun <fjun2008@gmail.com>
2016-04-05 09:30:20 +0800

    Exclude when compiling.

--- src/algorithms/P100SameTree.java
+++ src/algorithms/P100SameTree.java
@@ -12,7 +12,7 @@ public class P100SameTree {
     /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
+ *     int val
  *     TreeNode left;
  *     TreeNode right;
  *     TreeNode(int x) { val = x; }
