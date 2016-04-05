bd6eaacd68b7238e88151c2c0d41656f5ee33637
fjun <fjun2008@gmail.com>
2016-04-05 13:05:45 +0800

    268 MissingNumber

---
+++ src/algorithms/P268MissingNumber.java
@@ -0,0 +1,30 @@
+package algorithms;
+
+import javax.print.DocFlavor;
+
+/**
+ * Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+For example,
+Given nums = [0, 1, 3] return 2.
+
+Note:
+Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
+
+
+ * Created by fjun on 4/5/16.
+ */
+public class P268MissingNumber {
+    public static void main(String[] args) {
+        System.out.println(missingNumber(new int[]{0, 1, 3}));
+    }
+
+    public static int missingNumber(int[] nums) {
+        int sum = 0;
+        for (int n : nums) {
+            sum += n;
+        }
+        int l = nums.length;
+        return (1 + l) * l / 2 - sum;
+    }
+}
