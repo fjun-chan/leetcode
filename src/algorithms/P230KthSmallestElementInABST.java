b2c9582b7cabfb424c88e633bdac2daeeeebda56
fjun <fjun2008@gmail.com>
2016-04-07 00:12:25 +0800

    230 Kth Smallest Element in a BST. 328 Odd Even Linked List.

---
+++ src/algorithms/P230KthSmallestElementInABST.java
@@ -0,0 +1,71 @@
+package algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+ * <p>
+ * Note:
+ * You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+ * <p>
+ * Follow up:
+ * What if the BST is modified (insert/delete operations) often and you need to find the kth
+ *  smallest frequently? How would you optimize the kthSmallest routine?
+ * <p>
+ * Show Hint
+ *
+ * Created by fjun on 4/6/16.
+ */
+public class P230KthSmallestElementInABST {
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * 将左子树压入栈，并一层层pop。
+     */
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode n = root;
+        while(n.left!=null) {
+            stack.push(n);
+            n = n.left;
+        }
+
+        while(k>0 && (n!=null || !stack.isEmpty())) {
+            if(n==null) {
+                n = stack.pop();
+                if(--k==0) return n.val;
+                n = n.right;
+            } else {
+                stack.push(n);
+                n = n.left;
+            }
+        }
+        return n.val;
+    }
+
+    public int kthSmallestSlow(TreeNode root, int k) {
+        List<Integer> list = new ArrayList<Integer>();
+        inOrderTraversal(root, list);
+        return list.get(k - 1);
+    }
+
+    public void inOrderTraversal(TreeNode node, List<Integer> list) {
+        if (node == null) {
+            return;
+        }
+        inOrderTraversal(node.left, list);
+        list.add(node.val);
+        inOrderTraversal(node.right, list);
+    }
+}
---
+++ src/algorithms/P328OddEvenLinkedList.java
@@ -0,0 +1,64 @@
+package algorithms;
+
+/**
+ * Created by fjun on 4/6/16.
+ */
+public class P328OddEvenLinkedList {
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * 分裂成两个List，最后再合并起来。
+     */
+    public static ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return head;
+        }
+        ListNode evenList = head.next;
+        ListNode curOdd = head, curEven = head.next;
+        while (curEven != null && curEven.next != null) {
+            curOdd.next = curEven.next;
+            curOdd = curOdd.next;
+
+            curEven.next = curOdd.next;
+            curEven = curEven.next;
+        }
+        curOdd.next = evenList;
+        return head;
+    }
+
+    public static ListNode oddEvenListV1(ListNode head) {
+        int round = 1;
+        ListNode node = head;
+        while (node != null) {
+            ListNode next = node.next;
+            if (next == null) {
+                break;
+            }
+            ListNode parent = null;
+            ListNode found = next;
+            for (int i = 0; i < round; i ++) {
+                parent = found;
+                found = found.next;
+                if (found == null) {
+                    break;
+                }
+            }
+            if (found == null) {
+                break;
+            }
+            node.next = found;
+            parent.next = found.next;
+            found.next = next;
+            node = found;
+            round ++;
+        }
+        return head;
+    }
+}
