ec2e434b294a194333a2a749ad9d6df16bcc5dbe
fjun <fjun2008@gmail.com>
2016-04-16 18:34:14 +0800

    26 Remove Duplicates from Sorted Array.
    理解题意，需要删除重复，并且返回数组长度。并不只是返回长度。

--- src/algorithms/P26RemoveDuplicatesFromSortedArray.java
+++ src/algorithms/P26RemoveDuplicatesFromSortedArray.java
@@ -11,7 +11,7 @@ package algorithms;
  * Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the new length.
  * Created by fjun on 4/16/16.
  */
-public class P260RemoveDuplicatesFromSortedArray {
+public class P26RemoveDuplicatesFromSortedArray {
 
     public int removeDuplicates(int[] nums) {
         if (nums == null) return 0;
